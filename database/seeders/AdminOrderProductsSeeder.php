7e715a5cfd6496a88df1db112b00034f09348e58
Alexandr Bratko <59703515+alexandrbratko@users.noreply.github.com>
2021-08-16 14:35:32 +0300

    added database migrations and sedeers

---
+++ database/seeders/AdminOrderProductsSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AdminOrderProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
